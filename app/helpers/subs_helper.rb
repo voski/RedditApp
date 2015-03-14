1e56efebe49714896940ac4df7e5619b21860e7d
Narbe Voskanian <narbevoskanian@gmail.com>
2015-03-14 14:49:46 -0700

    move sub navbar into helper

--- app/helpers/subs_helper.rb
+++ app/helpers/subs_helper.rb
@@ -1,2 +1,34 @@
 module SubsHelper
+  def sub_nav_bar
+    html = <<-HTML
+      <div class="sub_nav_bar">
+        <ul>
+          <li>
+            <form action='#{new_post_url}' method="get">
+              <input type="hidden" name="post[sub_id]" value="#{@sub.id}">
+              <input type="submit" value="New Post">
+            </form>
+          </li>
+          <li>
+            #{button_to "All Subs", subs_url, method: :get}
+          </li>
+    HTML
+
+    if @sub.is_moderator?(current_user)
+      html += <<-HTML
+        <li>
+          #{button_to "Edit Sub", edit_sub_url, method: :get}
+        </li>
+        </ul>
+        </div>
+      HTML
+    else
+      html += <<-HTML
+        </ul>
+        </div>
+      HTML
+    end
+
+    html.html_safe
+  end
 end
