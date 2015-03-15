2c70831ebde620f7d6cf535277cf8e4c15751855
Narbe Voskanian <narbevoskanian@gmail.com>
2015-03-14 18:38:11 -0700

    Display comments on post page and added a nav bar

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,17 @@
 module PostsHelper
+  def post_nav_bar
+    html = <<-HTML
+      <div class="post_nav_bar">
+        <ul>
+          <li>
+            #{link_to "New comment", new_post_comment_url(@post)}
+          </li>
+          <li>
+            #{link_to "Back", :back}
+          </li>
+        </ul>
+        </div>
+      HTML
+    html.html_safe
+  end
 end
